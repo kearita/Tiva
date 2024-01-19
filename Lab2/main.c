c4fbb18b92bfaae9dcd16919240c2b932c20343a
Keara Taylor Lopez <kearatlopez210@gmail.com>
2024-01-18 21:29:33 -0500

    Initial commit

---
+++ Lab2/main.c
@@ -0,0 +1,20 @@
+/**
+ * main.c
+ * This program investigates how to change the frequency of the clock
+ * on the Tiva board, generate delays using the clock, and cycle through
+ * R, B, and G LEDs
+ */
+
+// ---standard libraries---
+#include <stdint.h>             // integer types and widths
+#include <stdbool.h>            // Boolean types
+// ---custom libraries---
+#include "inc/hw_types.h"       // common types
+#include "inc/hw_memmap.h"      // memory map for Tiva
+#include "driverlib/sysctl.h"   // system control driver
+#include "driverlib/gpio.h"     // GPIO API
+
+int main(void)
+{
+	return 0;
+}
